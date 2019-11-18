555ea3206e6048906b9f4d63938c016a82600c2f
tjforeman <tylerforeman92@gmail.com>
2019-11-18 13:12:23 -0500

    day2 complete

--- tylertest/settings.py
+++ tylertest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'notes',
+    'graphene_django'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'notes.schema.schema'
+}
